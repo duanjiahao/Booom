5f8317a612c33ca37e759b12dd25f03248ac3120
lys-0820 <silhouette223@outlook.com>
2024-04-22 17:24:08 +0800

    删除邮件类，修改药材、药方、药效类

--- Assets/Scripts/Logic/Base/RecipeUnit.cs
+++ Assets/Scripts/Logic/Base/RecipeUnit.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RecipeClassDefine
+public class RecipeUnit
 {
+    // 药方基本信息
     public int recipeID;
     public string desc;
     public Dictionary<string, int> herbInventory;
