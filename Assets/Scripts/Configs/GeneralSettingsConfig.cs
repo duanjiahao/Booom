48b2a5a897eb5769a1f46390d4f0cea15488aa8d
XieyishengJimmy <thanklifetime@gmail.com>
2024-05-02 23:56:08 +0800

    上传初始药材配置

--- Assets/Scripts/Configs/GeneralSettingsConfig.cs
+++ Assets/Scripts/Configs/GeneralSettingsConfig.cs
@@ -3,4 +3,6 @@ public class GeneralSettingsConfig : BaseConfig
     public int originalPrestige;
     public int prestigeCost;
     public int signTimes;
+    public int[] initialHerbs;
+    public int[] initialHerbsWeight;
 }
