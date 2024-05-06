372ad2a8cfc0dab0d2b55f1f050d1fc7a742c6aa
XieyishengJimmy <thanklifetime@gmail.com>
2024-05-06 15:08:26 +0800

    修改部分UI排列

--- Assets/Scripts/MarkHerbs/StartMarkMode.cs
+++ Assets/Scripts/MarkHerbs/StartMarkMode.cs
@@ -39,7 +39,7 @@ public class StartMarkMode : MonoBehaviour
         {
             if(DataManager.Instance.SignTimes > 0)
             {
-                Cursor.SetCursor(cursorTextureMark, Vector2.zero, CursorMode.Auto);
+                Cursor.SetCursor(cursorTextureMark, new Vector2(5,5), CursorMode.Auto);
                 MarkState.isOnMarkMode = true;
 
                 clickMask.SetActive(true);
